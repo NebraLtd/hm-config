0d2f2aba83f2ab4400e30ec04bd8f0ccb69307d0
Ryan Walmsley <ryan@ryanteck.uk>
2021-02-07 23:38:49 +0000

    Lets see if that improves visibilty.

--- start-gateway-config.sh
+++ start-gateway-config.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 git pull
 
+# Advertise on channels 37, 38 and 39
+echo 7 > /sys/kernel/debug/bluetooth/hci0/adv_channel_map
+# Send a beacon every 152.5 ms
+echo 153 > /sys/kernel/debug/bluetooth/hci0/adv_min_interval
+echo 153 > /sys/kernel/debug/bluetooth/hci0/adv_max_interval
+
 python3 main.py
