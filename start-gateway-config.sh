23e46b8412e543f394257a95865afa5e6f3e3336
Ryan Walmsley <ryan@ryanteck.uk>
2021-03-03 17:42:42 +0000

    Revert this function

--- start-gateway-config.sh
+++ start-gateway-config.sh
@@ -6,6 +6,6 @@ echo 7 > /sys/kernel/debug/bluetooth/hci0/adv_channel_map
 echo 153 > /sys/kernel/debug/bluetooth/hci0/adv_min_interval
 echo 153 > /sys/kernel/debug/bluetooth/hci0/adv_max_interval
 
-printf "pairable off\nquit" | /usr/bin/bluetoothctl
+#printf "pairable off\nquit" | /usr/bin/bluetoothctl
 
 python3 main.py
