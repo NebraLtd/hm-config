35ef9e67c49fc5f4a8f87e05f0c45dc01cd017d6
Ryan Walmsley <ryan@ryanteck.uk>
2021-03-03 12:35:41 +0000

    Disable BT pairing

--- start-gateway-config.sh
+++ start-gateway-config.sh
@@ -7,4 +7,6 @@ echo 7 > /sys/kernel/debug/bluetooth/hci0/adv_channel_map
 echo 153 > /sys/kernel/debug/bluetooth/hci0/adv_min_interval
 echo 153 > /sys/kernel/debug/bluetooth/hci0/adv_max_interval
 
+echo -e "pairable off\nquit" | /usr/bin/bluetoothctl
+
 python3 main.py
