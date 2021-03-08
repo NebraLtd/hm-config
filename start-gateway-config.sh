276e82fba5185538cdfbd1f3988f71bf7197a9c2
Ryan Walmsley <ryan@pi-supply.com>
2021-03-08 10:22:21 +0000

    Lets see if this works as a bluetooth resolution.

--- start-gateway-config.sh
+++ start-gateway-config.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-#git pull
+# Disable BluetoothD on the host
+dbus-send --system --dest=org.freedesktop.systemd1 --type=method_call --print-reply /org/freedesktop/systemd1   org.freedesktop.systemd1.Manager.StopUnit string:"bluetooth.service" string:"replace"
+
+sleep 1
+
+# Start BluetoothD in container
+
+bluetoothd -E -C &
+
+sleep 1
 
 # Advertise on channels 37, 38 and 39
 echo 7 > /sys/kernel/debug/bluetooth/hci0/adv_channel_map
 # Send a beacon every 152.5 ms
 echo 153 > /sys/kernel/debug/bluetooth/hci0/adv_min_interval
 echo 153 > /sys/kernel/debug/bluetooth/hci0/adv_max_interval
+
+# Disable pairing
 
 printf "pairable off\nquit" | /usr/bin/bluetoothctl
 
