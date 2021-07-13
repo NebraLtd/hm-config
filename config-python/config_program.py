c979267292d5d32052cd06d136fc83c546d21130
Ilya Starodubtsev <ilyastrodubtsev@gmail.com>
2021-07-13 13:28:14 +0600

    Ignoring error 901 for a large function

--- config-python/config_program.py
+++ config-python/config_program.py
@@ -347,7 +347,7 @@ class DiagnosticsCharacteristic(Characteristic):
         self.add_descriptor(opaqueStructure(self))
         self.p2pstatus = ""
 
-    def ReadValue(self, options):
+    def ReadValue(self, options): # noqa 901
         # TODO (Rob): come back and make this method less complex for
         # C901 complexity rules.
         logging.debug('Read diagnostics')
