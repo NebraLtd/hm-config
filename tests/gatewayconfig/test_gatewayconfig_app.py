cd45050059b898401cd4c3bfb272b9f53f8c1fbd
Marvin Arnold <marvin@geeky.rocks>
2021-08-25 13:32:34 -0500

    fix: mock bluetooth during test

--- tests/gatewayconfig/test_gatewayconfig_app.py
+++ tests/gatewayconfig/test_gatewayconfig_app.py
@@ -13,8 +13,11 @@ ETHO_FILE_MOCK = 'A1:B2:C3:DD:E5:F6'
 
 class TestGatewayconfigSha(TestCase):
 
+    @patch('lib.cputemp.bletools.BleTools.get_bus')
+    @patch('lib.cputemp.bletools.BleTools.find_adapter')
+    @patch('dbus.Interface')
     @patch('builtins.open', new_callable=mock_open, read_data=ETHO_FILE_MOCK)
-    def test_gpio_pins(self, mock_file):
+    def test_gpio_pins(self, mock_dbus_interface, mock_findadapter, mock_getbus, mock_file):
         app = GatewayconfigApp('https://11111111111111119f8b0c9b118c415a@o111111.ingest.sentry.io/1111111', 'BALENA_APP_NAME', 'BALENA_DEVICE_UUID', 'NEBHNT-IN1',
         'ETH0_MOCK_USED', 'WLAN0_MAC_ADDRESS_FILEPATH', 'MINER_KEYS_FILEPATH', 'DIAGNOSTICS_JSON_URL', 'ETHERNET_IS_ONLINE_FILEPATH', 'FIRMWARE_VERSION')
 
