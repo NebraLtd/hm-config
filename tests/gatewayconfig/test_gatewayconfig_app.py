659c4a2df22cc879cc36873a6b297f74b56f4246
Viktor Petersson <self@vpetersson.com>
2021-10-15 17:55:44 +0100

    Remove unused argument

--- tests/gatewayconfig/test_gatewayconfig_app.py
+++ tests/gatewayconfig/test_gatewayconfig_app.py
@@ -19,7 +19,7 @@ class TestGatewayconfigSha(TestCase):
     @patch('builtins.open', new_callable=mock_open, read_data=ETHO_FILE_MOCK)
     def test_gpio_pins(self, mock_dbus_interface, mock_findadapter, mock_getbus, mock_file):
         app = GatewayconfigApp('https://11111111111111119f8b0c9b118c415a@o111111.ingest.sentry.io/1111111', 'BALENA_APP_NAME', 'BALENA_DEVICE_UUID', 'NEBHNT-IN1',
-        'ETH0_MOCK_USED', 'WLAN0_MAC_ADDRESS_FILEPATH', 'MINER_KEYS_FILEPATH', 'DIAGNOSTICS_JSON_URL', 'ETHERNET_IS_ONLINE_FILEPATH', 'FIRMWARE_VERSION')
+        'ETH0_MOCK_USED', 'WLAN0_MAC_ADDRESS_FILEPATH', 'DIAGNOSTICS_JSON_URL', 'ETHERNET_IS_ONLINE_FILEPATH', 'FIRMWARE_VERSION')
 
         self.assertEqual(app.variant, 'NEBHNT-IN1')
         self.assertEqual(app.get_button_pin(), 26)
