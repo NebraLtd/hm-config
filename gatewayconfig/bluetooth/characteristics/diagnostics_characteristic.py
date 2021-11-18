83b6fa9cbff553dec9dbd76b4a12efb3d522d88b
Zhang <zpoterasu@nebra.com>
2021-11-17 22:12:27 -0500

    fix: make DiagnosticsCharacteristic return correct protobuf always

--- gatewayconfig/bluetooth/characteristics/diagnostics_characteristic.py
+++ gatewayconfig/bluetooth/characteristics/diagnostics_characteristic.py
@@ -16,6 +16,7 @@ DBUS_UNAVAILABLE_VALUE = "Loading..."
 DBUS_LOAD_SLEEP_SECONDS = 0.1
 logger = get_logger(__name__)
 
+
 class DiagnosticsCharacteristic(Characteristic):
     # Returns proto of eth, wifi, fw, ip, p2pstatus
 
@@ -25,19 +26,58 @@ class DiagnosticsCharacteristic(Characteristic):
                 ["read"], service)
         self.add_descriptor(DiagnosticsDescriptor(self))
         self.add_descriptor(OpaqueStructureDescriptor(self))
-        self.p2pstatus = False
-        self.eth0_mac_address = eth0_mac_address
-        self.wlan0_mac_address = wlan0_mac_address
+
+        firmware_version = os.getenv('FIRMWARE_VERSION')
+        self.new_diagnostics_proto(eth0_mac_address, wlan0_mac_address,
+                                   firmware_version)
 
     def ReadValue(self, options):
         logger.debug('Read diagnostics')
         try:
-            self.p2pstatus = self.get_p2pstatus()
-            diagnostics = self.build_diagnostics_proto().SerializeToString()
-            logger.debug("Diagnostics are %s" % diagnostics)
-            return string_to_dbus_byte_array(diagnostics)
-        except Exception:
-            logger.exception("Unexpected exception while trying to read diagnostics")
+            p2pstatus = self.get_p2pstatus()
+
+            # Update self.diagnostics_proto
+            self.update_diagnostics_proto(p2pstatus)
+        except Exception as ex:
+            logger.exception("Unexpected exception while trying to read diagnostics: %s" % str(ex))
+
+        diagnostics = self.diagnostics_proto.SerializeToString()
+        logger.debug("Diagnostics are %s" % diagnostics)
+        return string_to_dbus_byte_array(diagnostics)
+
+    # Make a new diagnostics_pb2.diagnostics_v1
+    def new_diagnostics_proto(self,
+                              eth0_mac_address,
+                              wlan0_mac_address,
+                              firmware_version):
+        logger.debug('New Diagnostics Proto')
+
+        self.diagnostics_proto = diagnostics_pb2.diagnostics_v1()
+        self.diagnostics_proto.diagnostics['connected'] = DBUS_UNAVAILABLE_VALUE
+        self.diagnostics_proto.diagnostics['dialable'] = DBUS_UNAVAILABLE_VALUE
+        self.diagnostics_proto.diagnostics['height'] = DBUS_UNAVAILABLE_VALUE
+        self.diagnostics_proto.diagnostics['nat_type'] = DBUS_UNAVAILABLE_VALUE
+
+        self.diagnostics_proto.diagnostics['eth'] = eth0_mac_address.replace(":", "")
+        self.diagnostics_proto.diagnostics['wifi'] = wlan0_mac_address.replace(":", "")
+        self.diagnostics_proto.diagnostics['fw'] = str(firmware_version)
+        self.diagnostics_proto.diagnostics['ip'] = ""
+
+    # Update diagnostics_proto member variable
+    def update_diagnostics_proto(self, p2pstatus):
+        logger.debug('Update Diagnostics Proto')
+
+        if p2pstatus:
+            try:
+                self.diagnostics_proto.diagnostics['connected'] = str(p2pstatus[0][1])
+                self.diagnostics_proto.diagnostics['dialable'] = str(p2pstatus[1][1])
+                self.diagnostics_proto.diagnostics['height'] = str(p2pstatus[3][1])
+                self.diagnostics_proto.diagnostics['nat_type'] = str(p2pstatus[2][1])
+            except Exception as ex:
+                logger.exception("Unexpected exception while trying to read p2pstatus")
+                raise ex
+
+        self.diagnostics_proto.diagnostics['ip'] = self.get_ip()
 
     # Returns the p2pstatus or an empty string if there is a dbus failure
     def get_p2pstatus(self):
@@ -50,39 +90,19 @@ class DiagnosticsCharacteristic(Characteristic):
         miner_interface = dbus.Interface(miner_object, 'com.helium.Miner')
         sleep(DBUS_LOAD_SLEEP_SECONDS)
         logger.debug('Diagnostics p2pstatus')
-            
+
         try:
             p2pstatus = miner_interface.P2PStatus()
             logger.debug('DBUS P2P SUCCEED')
-            logger.debug(self.p2pstatus)
-        except dbus.exceptions.DBusException:
+            logger.debug(p2pstatus)
+        except dbus.exceptions.DBusException as ex:
             p2pstatus = False
             logger.warn('DBUS P2P FAIL')
+            raise ex
 
         logger.debug("p2pstatus: %s" % p2pstatus)
         return p2pstatus
 
-    # Returns a diagnostics_pb2.diagnostics_v1
-    def build_diagnostics_proto(self):
-        diagnostics_proto = diagnostics_pb2.diagnostics_v1()
-        if not self.p2pstatus:
-            diagnostics_proto.diagnostics['connected'] = DBUS_UNAVAILABLE_VALUE
-            diagnostics_proto.diagnostics['dialable'] = DBUS_UNAVAILABLE_VALUE
-            diagnostics_proto.diagnostics['height'] = DBUS_UNAVAILABLE_VALUE
-            diagnostics_proto.diagnostics['nat_type'] = DBUS_UNAVAILABLE_VALUE
-        else:
-            diagnostics_proto.diagnostics['connected'] = str(self.p2pstatus[0][1])
-            diagnostics_proto.diagnostics['dialable'] = str(self.p2pstatus[1][1])
-            diagnostics_proto.diagnostics['height'] = str(self.p2pstatus[3][1])
-            diagnostics_proto.diagnostics['nat_type'] = str(self.p2pstatus[2][1])
-
-        diagnostics_proto.diagnostics['eth'] = self.eth0_mac_address.replace(":", "")
-        diagnostics_proto.diagnostics['wifi'] = self.wlan0_mac_address.replace(":", "")
-        diagnostics_proto.diagnostics['fw'] = os.getenv('FIRMWARE_VERSION')
-        diagnostics_proto.diagnostics['ip'] = self.get_ip()
-        logger.debug('All attibutes were added to the diagnostics proto')
-        return diagnostics_proto
-    
     # Return the ETH IP address, or WLAN if it does not exist
     # 0.0.0.0 is the default value if neither ETH or WLAN IP available
     def get_ip(self):
